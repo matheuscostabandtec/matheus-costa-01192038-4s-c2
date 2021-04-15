ac1988ffe852836295352cf64fcb1f4426cc9748
matheuscostabandtec <matheus.costa@bandtec.com.br>
2021-04-14 22:55:54 -0300

    add:prazo acabou mas as partes funcionais funcionando pelo menos

--- ApiCachorros/app/src/main/java/com/example/apicachorros/MainActivity.kt
+++ ApiCachorros/app/src/main/java/com/example/apicachorros/MainActivity.kt
@@ -39,11 +39,43 @@ class MainActivity : AppCompatActivity() {
 
         if(swFiltroAmigavel.isChecked){
             val listaCachorrosAmigaveis = mutableListOf<Cachorros>()
+            val telaErro = Intent(this, TelaError::class.java)
+            val telaResultado = Intent(this, TelaResultado::class.java)
+
             apiCachorros.getTodos().enqueue(object: Callback<List<Cachorros>> {
                 override fun onResponse(call: Call<List<Cachorros>>, response: Response<List<Cachorros>>){
                    val dogsFiltrados = response.body()?.filter { c -> c.indicadoCriancas}
                     if(dogsFiltrados !== null){
                         dogsFiltrados.forEach { c -> listaCachorrosAmigaveis.add(c) }
+                    }
+
+                    val cachorrosEncontrados = listaCachorrosAmigaveis.filter { c -> c.id == id1 || c.id == id2 }
+
+                    if (cachorrosEncontrados.isEmpty()){
+                        telaErro.putExtra("id1", id1)
+                        telaErro.putExtra("id2", id2)
+                        startActivity(telaErro)
+                    } else {
+                        var valorTotal = 0
+                        cachorrosEncontrados.forEach { c -> valorTotal += c.precoMedio }
+
+                        if(cachorrosEncontrados.size == 2){
+                            telaResultado.putExtra("id1", id1)
+                            telaResultado.putExtra("id2", id2)
+                        } else {
+                            val cachorroId1 = cachorrosEncontrados.find { c -> c.id == id1 }
+                            val cachorroId2 = cachorrosEncontrados.find { c -> c.id == id2 }
+
+                            if (cachorroId1 !== null && cachorroId2 == null){
+                                telaResultado.putExtra("id1", id1)
+                                telaResultado.putExtra("id2", 0)
+                            } else {
+                                telaResultado.putExtra("id1", 0)
+                                telaResultado.putExtra("id2", id2)
+                            }
+                        }
+                        telaResultado.putExtra("total", valorTotal)
+                        startActivity(telaResultado)
                     }
                 }
 
@@ -53,40 +85,9 @@ class MainActivity : AppCompatActivity() {
                 }
             })
 
-            Toast.makeText(this, "essa é a lista $listaCachorrosAmigaveis", Toast.LENGTH_SHORT).show()
 
-            val cachorrosEncontrados = listaCachorrosAmigaveis.filter { c -> c.id == id1 || c.id == id2 }
-
-            if (cachorrosEncontrados.isEmpty()){
-                val telaErro = Intent(this, TelaError::class.java)
-                telaErro.putExtra("id1", id1)
-                telaErro.putExtra("id2", id2)
-                startActivity(telaErro)
-            } else {
-                val telaResultado = Intent(this, TelaResultado::class.java)
-                var valorTotal = 0
-                cachorrosEncontrados.forEach { c -> valorTotal += c.precoMedio }
-
-                if(cachorrosEncontrados.size == 2){
-                    telaResultado.putExtra("id1", id1)
-                    telaResultado.putExtra("id2", id2)
-                } else {
-                    val cachorroId1 = cachorrosEncontrados.find { c -> c.id == id1 }
-                    val cachorroId2 = cachorrosEncontrados.find { c -> c.id == id2 }
-
-                    if (cachorroId1 !== null && cachorroId2 == null){
-                        telaResultado.putExtra("id1", id1)
-                        telaResultado.putExtra("id2", 0)
-                    } else {
-                        telaResultado.putExtra("id1", 0)
-                        telaResultado.putExtra("id2", id2)
-                    }
-                }
-                telaResultado.putExtra("total", valorTotal)
-                startActivity(telaResultado)
-            }
         } else {
-            val telaResultado = Intent(this, TelaResultado::class.java)
+            precoTotal = 0
             apiCachorros.getById(id1).enqueue(object : Callback<Cachorros> {
                 override fun onResponse(call: Call<Cachorros>, response: Response<Cachorros>) {
                     val cachorro = response.body()
@@ -94,45 +95,48 @@ class MainActivity : AppCompatActivity() {
                         id1Encontrado = true
                         precoTotal += cachorro.precoMedio
                     }
-                }
 
+                    apiCachorros.getById(id2).enqueue(object : Callback<Cachorros> {
+                        override fun onResponse(call: Call<Cachorros>, response: Response<Cachorros>) {
+                            val cachorro = response.body()
+                            if (cachorro != null) {
+                                id2Encontrado = true
+                                precoTotal += cachorro.precoMedio
+                            }
+                            verificarResultado(id1,id2)
+                        }
+                        override fun onFailure(call: Call<Cachorros>, t: Throwable) {
+                            Toast.makeText(baseContext, "Erro: ${t.message!!}", Toast.LENGTH_SHORT).show()
+                        }
+                    })
+                }
                 override fun onFailure(call: Call<Cachorros>, t: Throwable) {
                     Toast.makeText(baseContext, "Erro: ${t.message!!}", Toast.LENGTH_SHORT).show()
                 }
+
             })
 
-            apiCachorros.getById(id2).enqueue(object : Callback<Cachorros> {
-                override fun onResponse(call: Call<Cachorros>, response: Response<Cachorros>) {
-                    val cachorro = response.body()
-                    if (cachorro != null) {
-                        id2Encontrado = true
-                        precoTotal += cachorro.precoMedio
-                    }
-                }
-
-                override fun onFailure(call: Call<Cachorros>, t: Throwable) {
-                    Toast.makeText(baseContext, "Erro: ${t.message!!}", Toast.LENGTH_SHORT).show()
-                }
-            })
-
-            if (!id1Encontrado && !id2Encontrado){
-                val telaErro = Intent(this, TelaError::class.java)
-                telaErro.putExtra("id1", id1)
-                telaErro.putExtra("id2", id2)
-                startActivity(telaErro)
-            } else if (id1Encontrado && !id2Encontrado){
-                telaResultado.putExtra("id1", id1)
-                telaResultado.putExtra("id2", 0)
-            } else if (!id1Encontrado && id2Encontrado) {
-                telaResultado.putExtra("id1", 0)
-                telaResultado.putExtra("id2", id1)
-            } else {
-                telaResultado.putExtra("id1", id1)
-                telaResultado.putExtra("id2", id2)
-            }
-            telaResultado.putExtra("total", precoTotal)
-            startActivity(telaResultado)
         }
+    }
 
+   fun verificarResultado(id1:Int, id2:Int){
+       val telaResultado = Intent(this, TelaResultado::class.java)
+       if (!id1Encontrado && !id2Encontrado){
+           val telaErro = Intent(this, TelaError::class.java)
+           telaErro.putExtra("id1", id1)
+           telaErro.putExtra("id2", id2)
+           startActivity(telaErro)
+       } else if (id1Encontrado && !id2Encontrado){
+           telaResultado.putExtra("id1", id1)
+           telaResultado.putExtra("id2", 0)
+       } else if (!id1Encontrado && id2Encontrado) {
+           telaResultado.putExtra("id1", 0)
+           telaResultado.putExtra("id2", id1)
+       } else {
+           telaResultado.putExtra("id1", id1)
+           telaResultado.putExtra("id2", id2)
+       }
+       telaResultado.putExtra("total", precoTotal)
+       startActivity(telaResultado)
     }
 }
